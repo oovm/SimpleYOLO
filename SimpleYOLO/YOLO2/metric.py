b87e98c20160804e8d7f32572073ee8482036956
Aster <galaster@foxmail.com>
2019-02-06 12:39:35 +0800

    算法::YOLO2 Loss function

---
+++ SimpleYOLO/YOLO2/metric.py
@@ -0,0 +1,18 @@
+import mxnet as mx
+
+
+class LossRecorder(mx.metric.EvalMetric):
+    """LossRecorder is used to record raw loss so we can observe loss directly
+    """
+
+    def __init__(self, name):
+        super(LossRecorder, self).__init__(name)
+
+    def update(self, labels, preds=0):
+        """Update metric with pure loss
+        """
+        for loss in labels:
+            if isinstance(loss, mx.nd.NDArray):
+                loss = loss.asnumpy()
+            self.sum_metric += loss.sum()
+            self.num_inst += 1
